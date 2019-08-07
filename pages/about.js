b418ef4deaf4872059fb9fdeb3d1ce1901ec0b8e
dasolpark <dasol.park@monimedia.com>
2019-08-07 15:27:49 +0800

    style jsx

--- pages/about.js
+++ pages/about.js
@@ -3,9 +3,15 @@ import Layout from '../components/Layout';
 const About = () => {
   return (
     <Layout>
+      <style jsx>
+        {`
+          h2 {
+            color: green
+          }
+        `}
+      </style>
       <h2>Hello, I'm Dasol</h2>
     </Layout>
-
   )
 }
 
