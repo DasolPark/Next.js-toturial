85aab91111329e7061f443f427c03f6136c14f06
dasolpark <dasol.park@monimedia.com>
2019-08-07 15:11:16 +0800

    SSR AJAX with axios

--- pages/ssr-test.js
+++ pages/ssr-test.js
@@ -1,16 +1,26 @@
 import Layout from '../components/Layout';
+import axios from 'axios';
 
 class SSRTest extends React.Component {
   static async getInitialProps({ req }) {
-    return req
-      ? { from: 'server' }
-      : { from: 'client' }
+    const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+    return {
+      users: response.data
+    }
   }
 
   render() {
+    const { users } = this.props;
+
+    const userList = users.map(
+      user => <li key={user.id}>{user.username}</li>
+    )
+
     return (
       <Layout>
-        excuted from {this.props.from}
+        <ul>
+          {userList}
+        </ul>
       </Layout>
     )
   }
